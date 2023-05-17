ff1741ac6f36fca6495507448523a54fb6f3237c
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-05-17 09:56:10 +0600

    skillbar done

--- comments/Skillbar.js
+++ comments/Skillbar.js
@@ -1,21 +1,49 @@
-import styles from '@/styles/skillbar.module.css';
+import styles from "@/styles/skillbar.module.css";
 const Skillbar = () => {
-    return (
-        <div>
-            <div className={styles.skill}>
-            <h2 className={styles.titleText}>Skill Bar Component</h2>
-            <div className={styles.skillBox}>
-                <span className={styles.title}> HTML</span>
-                <div className={`${styles.skillBar} ${styles.html}`}>
-                    <span className={styles.skillPer}> 
-                    <span className={styles.tooltip}>90%</span>
-                    </span>
-                </div>
-                
-            </div>
-            </div>
+  return (
+    <div>
+      <div className={styles.skill}>
+        <h2 className={styles.titleText}>Skill Bar Component</h2>
+        <div className={styles.skillBox}>
+          {/* html section  */}
+          <span className={styles.title}> HTML</span>
+          <div className={styles.skillBar}>
+            <span className= {`${styles.skillPer} ${styles.html}`}>
+              <span className={styles.tooltip}>90%</span>
+            </span>
+          </div>
+          {/* css section  */}
+          <span className={styles.title}> css</span>
+          <div className={styles.skillBar}>
+            <span className={`${styles.skillPer} ${styles.css}`}>
+              <span className={styles.tooltip}>80%</span>
+            </span>
+          </div>
+          {/* javascript section  */}
+          <span className={styles.title}> Javascript</span>
+          <div className={styles.skillBar}>
+            <span className={`${styles.skillPer} ${styles.javascript}`}>
+              <span className={styles.tooltip}>75%</span>
+            </span>
+          </div>
+          {/* node section  */}
+          <span className={styles.title}> Node</span>
+          <div className={`${styles.skillBar}`}>
+            <span className={`${styles.skillPer} ${styles.node}`}>
+              <span className={styles.tooltip}>60%</span>
+            </span>
+          </div>
+          {/* Mongodb section  */}
+          <span className={styles.title}> Mongodb</span>
+          <div className={`${styles.skillBar}`}>
+            <span className={`${styles.skillPer} ${styles.mongodb}`}>
+              <span className={styles.tooltip}>90%</span>
+            </span>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Skillbar;
