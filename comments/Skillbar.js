184cb6287c660ce442f3a2305d790b439c66e280
MollikaComputer Provider <mollikacomputer3@gmail.com>
2023-05-17 08:59:28 +0600

    html skillbar done

--- comments/Skillbar.js
+++ comments/Skillbar.js
@@ -2,7 +2,18 @@ import styles from '@/styles/skillbar.module.css';
 const Skillbar = () => {
     return (
         <div>
+            <div className={styles.skill}>
             <h2 className={styles.titleText}>Skill Bar Component</h2>
+            <div className={styles.skillBox}>
+                <span className={styles.title}> HTML</span>
+                <div className={`${styles.skillBar} ${styles.html}`}>
+                    <span className={styles.skillPer}> 
+                    <span className={styles.tooltip}>90%</span>
+                    </span>
+                </div>
+                
+            </div>
+            </div>
         </div>
     );
 };
--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,9 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main>
+    <>
       <h2 className="text-2xl">Skill Bar Home page</h2>
       <Skillbar/>
-    </main>
+    </>
   )
 }
